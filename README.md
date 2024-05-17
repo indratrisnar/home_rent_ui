87bf53ec84417b09ddd65c35cd64239a586882db
indratrisnar <trisnaindra1998@gmail.com>
2024-05-17 19:14:45 +0700

    done

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 Home Rent App  
 https://www.figma.com/community/file/992789948125883979
+
+[Tutorial](https://youtu.be/qc4lr79gvdM)
+
+|                         Menu                         |                         Home                         |                         Detail                         |
+| :--------------------------------------------------: | :--------------------------------------------------: | :----------------------------------------------------: |
+| <img src="screenshot/menu.png" style="height:450px"> | <img src="screenshot/home.png" style="height:450px"> | <img src="screenshot/detail.png" style="height:450px"> |
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:home_rent_ui/models/home.dart';
+import 'package:home_rent_ui/pages/detail_page.dart';
+import 'package:home_rent_ui/pages/menu_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +13,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xffFAFAFA),
+        textTheme: GoogleFonts.ralewayTextTheme(),
       ),
+      home: const MenuPage(),
+      routes: {
+        '/detail': (context) {
+          Home home = ModalRoute.of(context)!.settings.arguments as Home;
+          return DetailPage(home: home);
+        }
+      },
     );
   }
 }
